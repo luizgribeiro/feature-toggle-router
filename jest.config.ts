98e84916caab8a56cbd841950d37c11e6b65eb9c
Luiz Ribeiro <ltrindaderibeiro@gmail.com>
2023-09-03 12:36:50 -0300

    chore: use swc for tests transpilation
    Signed-off-by: Luiz Ribeiro <ltrindaderibeiro@gmail.com>

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,11 @@
 import type {Config} from 'jest';
 
 const config: Config = {
+  transform: {
+    "^.+\\.(t|j)sx?$": "@swc/jest",
+  },
   verbose: true,
+  testEnvironment: 'node',
 };
 
 export default config;
